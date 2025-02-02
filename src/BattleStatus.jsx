a38150e3af3ad828f2a256328f29e6097b70ddc7
Jason Smith <jasondsmith2501@gmail.com>
2025-02-02 11:06:22 -0800

    Added battle status messages based on health

--- src/BattleStatus.jsx
+++ src/BattleStatus.jsx
@@ -1,9 +1,21 @@
 import "./BattleStatus.css";
 
-function BattleStatus({ message }) {
+function BattleStatus({ playerHealth, enemyHealth }) {
+    let statusMessage;
+
+    if (playerHealth > 0 && enemyHealth > 0) {
+        statusMessage = "Engage the enemy! ☄️";
+    } else if (playerHealth === 0 && enemyHealth === 0) {
+        statusMessage = "It's a draw! 🤝 Both spacecrafts haave been neutralized.";
+    } else if (playerHealth === 0) {
+        statusMessage = "Mission Failed. 😮 Your spacecraft has been defeated.";
+    } else {
+        statusMessage = "Congratulations! 😎💪 You've successfully defended your spacecraft."
+    }
+
     return (
         <div class="Battle-Status">
-            <p>{message}</p>
+            <p>{statusMessage}</p>
         </div>
     );
 }
--- src/SpaceBattleSimulator.jsx
+++ src/SpaceBattleSimulator.jsx
@@ -3,19 +3,22 @@ import PlayerHealth from "./PlayerHealth";
 import "./SpaceBattleSimulator.css";
 
 function SpaceBattleSimulator({ }) {
+    let enemyHealth = 0;
+    let playerHealth = 80;
+
     return (
         <div class="Space-Battle-Simulator">
             <div>
                 <h1 class="Space-Battle-Simulator-Header">Space Battle Simulator</h1>
             </div>
             <div class="Space-Battle-Simulator-Health-Actions">
-                <PlayerHealth name={"Player"} health={100} color={"green"} />
+                <PlayerHealth name={"Player"} health={playerHealth} color={"green"} />
                 <div>
                     <button>Fire!</button>
                 </div>
-                <PlayerHealth name={"Enemy"} health={80} color={"red"} />
+                <PlayerHealth name={"Enemy"} health={enemyHealth} color={"red"} />
             </div>
-            <BattleStatus message={"Congratulations!"} />
+            <BattleStatus playerHealth={playerHealth} enemyHealth={enemyHealth} />
         </div>
     );
 }
