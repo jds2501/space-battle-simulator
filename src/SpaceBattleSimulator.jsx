db6ae109664f808723bb8677de8db44e6d3841fa
Jason Smith <jasondsmith2501@gmail.com>
2025-02-02 16:08:10 -0800

    Added fire & restart handlers

--- src/SpaceBattleSimulator.jsx
+++ src/SpaceBattleSimulator.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
 import BattleStatus from "./BattleStatus";
 import PlayerAction from "./PlayerAction";
 import PlayerHealth from "./PlayerHealth";
 import "./SpaceBattleSimulator.css";
 
-function SpaceBattleSimulator({ }) {
-    let enemyHealth = 0;
-    let playerHealth = 80;
+function getRandomNum(range) {
+    return Math.floor(Math.random() * range);
+}
+
+function getResultingHealth(currentHealth, damage) {
+    let newPlayerHealth = currentHealth - damage;
+
+    if (newPlayerHealth < 0) {
+        newPlayerHealth = 0;
+    }
+
+    return newPlayerHealth;
+}
+
+function SpaceBattleSimulator({ playerDamageRange = 30, enemyDamageRange = 30 }) {
+    const [enemyHealth, setEnemyHealth] = useState(100);
+    const [playerHealth, setPlayerHealth] = useState(100);
+
+    function fire() {
+        const playerDamage = getRandomNum(playerDamageRange);
+        setPlayerHealth(playerHealth => getResultingHealth(playerHealth, playerDamage));
+
+        const enemyDamage = getRandomNum(enemyDamageRange);
+        setEnemyHealth(enemyHealth => getResultingHealth(enemyHealth, enemyDamage));
+    }
+
+    function restart() {
+        setEnemyHealth(100);
+        setPlayerHealth(100);
+    }
+
+    function action() {
+        if (enemyHealth === 0 || playerHealth === 0) {
+            restart();
+        } else {
+            fire();
+        }
+    }
 
     return (
         <div className="Space-Battle-Simulator">
@@ -14,9 +50,7 @@ function SpaceBattleSimulator({ }) {
             </div>
             <div className="Space-Battle-Simulator-Health-Actions">
                 <PlayerHealth name={"Player"} health={playerHealth} color={"green"} />
-                <PlayerAction playerHealth={playerHealth} enemyHealth={enemyHealth} actionCallback={() => {
-                    console.log("Click!");
-                }} />
+                <PlayerAction playerHealth={playerHealth} enemyHealth={enemyHealth} actionCallback={action} />
                 <PlayerHealth name={"Enemy"} health={enemyHealth} color={"red"} />
             </div>
             <BattleStatus playerHealth={playerHealth} enemyHealth={enemyHealth} />
